790162b3307ba55b19737343c2aef63f455788e4
nidnogg <henriquevt98@gmail.com>
2024-02-14 01:39:26 -0300

    Added country code to currency code consts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import mockCurrencyData from './temp/mock_currency_data.json'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedCountry, setSelectedCountry] = useState('')
+  const [baseSlotValues, setBaseSlotValues] = useState([0, 0, 0, 0, 0])
+
   const currencyData = mockCurrencyData.data
+
+  const handleSlotChange = (index: number, value: number) => {
+    const newSlotValues = [...baseSlotValues]
+    newSlotValues[index] = value
+    setBaseSlotValues(newSlotValues)
+  }
+
   useEffect(() => {
     console.log(currencyData)
   })
@@ -17,7 +24,56 @@ function App() {
         <p className="subtitle">
           Compare currencies based on values you find expensive or cheap.
         </p>
-        <p>Start by selecting your home country</p>
+        {selectedCountry && (
+          <p>
+            Currency Code: {currencyData[selectedCountry].currency_code}
+          </p>
+        )}
+        {selectedCountry && (
+          <div className="ruler">
+            <input
+              type="number"
+              value={baseSlotValues[0]}
+              onChange={(e) => handleSlotChange(0, parseInt(e.target.value))}
+            />
+            <input
+              type="number"
+              value={baseSlotValues[1]}
+              onChange={(e) => handleSlotChange(1, parseInt(e.target.value))}
+            />
+            <input
+              type="number"
+              value={baseSlotValues[2]}
+              onChange={(e) => handleSlotChange(2, parseInt(e.target.value))}
+            />
+            <input
+              type="number"
+              value={baseSlotValues[3]}
+              onChange={(e) => handleSlotChange(3, parseInt(e.target.value))}
+            />
+            <input
+              type="number"
+              value={baseSlotValues[4]}
+              onChange={(e) => handleSlotChange(4, parseInt(e.target.value))}
+            />
+          </div>
+        )}
+        {!selectedCountry && (
+          <div>
+            <p>Start by selecting your home country</p>
+            <select
+              value={selectedCountry}
+              onChange={(e) => setSelectedCountry(e.target.value)}
+            >
+              <option value="">Select Country</option>
+              {Object.keys(currencyData).map((country) => (
+                <option key={country} value={country}>
+                  {currencyData[country].country}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
       </div>
     </>
   )
---
+++ src/consts.tsx
@@ -0,0 +1,261 @@
+export const countryCurrencyCodes: Record<string, string> = {
+  'AF': 'AFN', // AFGHANISTAN
+  'AL': 'ALL', // ALBANIA
+  'DZ': 'DZD', // ALGERIA
+  'AS': 'USD', // AMERICAN SAMOA
+  'AD': 'EUR', // ANDORRA
+  'AO': 'AOA', // ANGOLA
+  'AI': 'XCD', // ANGUILLA
+  // 'AQ': '', // ANTARCTICA (No universal currency)
+  'AG': 'XCD', // ANTIGUA AND BARBUDA
+  'AR': 'ARS', // ARGENTINA
+  'AM': 'AMD', // ARMENIA
+  'AW': 'AWG', // ARUBA
+  'AU': 'AUD', // AUSTRALIA
+  'AT': 'EUR', // AUSTRIA
+  'AZ': 'AZN', // AZERBAIJAN
+  'BS': 'BSD', // BAHAMAS (THE)
+  'BH': 'BHD', // BAHRAIN
+  'BD': 'BDT', // BANGLADESH
+  'BB': 'BBD', // BARBADOS
+  'BY': 'BYN', // BELARUS
+  'BE': 'EUR', // BELGIUM
+  'BZ': 'BZD', // BELIZE
+  'BJ': 'XOF', // BENIN
+  'BM': 'BMD', // BERMUDA
+  'BT': 'BTN', // BHUTAN
+  // 'IN': 'INR', // BHUTAN (Indian Rupee)
+  'BO': 'BOB', // BOLIVIA (PLURINATIONAL STATE OF)
+  'BOV': 'BOV', // BOLIVIA (PLURINATIONAL STATE OF) - Mvdol
+  // 'AN': '', // BONAIRE, SINT EUSTATIUS AND SABA
+  'BA': 'BAM', // BOSNIA AND HERZEGOVINA
+  'BW': 'BWP', // BOTSWANA
+  'BV': 'NOK', // BOUVET ISLAND
+  'BR': 'BRL', // BRAZIL
+  // 'IO': '', // BRITISH INDIAN OCEAN TERRITORY (THE)
+  'BN': 'BND', // BRUNEI DARUSSALAM
+  'BG': 'BGN', // BULGARIA
+  'BF': 'XOF', // BURKINA FASO
+  'BI': 'BIF', // BURUNDI
+  'CV': 'CVE', // CABO VERDE
+  'KH': 'KHR', // CAMBODIA
+  'CM': 'XAF', // CAMEROON
+  'CA': 'CAD', // CANADA
+  'KY': 'KYD', // CAYMAN ISLANDS (THE)
+  'CF': 'XAF', // CENTRAL AFRICAN REPUBLIC (THE)
+  'TD': 'XAF', // CHAD
+  'CLF': 'CLF', // CHILE - Unidad de Fomento
+  'CL': 'CLP', // CHILE - Chilean Peso
+  'CN': 'CNY', // CHINA
+  'CX': 'AUD', // CHRISTMAS ISLAND
+  'CC': 'AUD', // COCOS (KEELING) ISLANDS (THE)
+  'CO': 'COP', // COLOMBIA
+  'COU': 'COU', // COLOMBIA - Unidad de Valor Real
+  'KM': 'KMF', // COMOROS (THE)
+  'CD': 'CDF', // CONGO (THE DEMOCRATIC REPUBLIC OF THE)
+  'CG': 'XAF', // CONGO (THE)
+  'CK': 'NZD', // COOK ISLANDS (THE)
+  'CR': 'CRC', // COSTA RICA
+  'HR': 'EUR', // CROATIA
+  'CU': 'CUP', // CUBA
+  'CUC': 'CUC', // CUBA - Peso Convertible
+  'CW': 'ANG', // CURAÇAO
+  'CY': 'EUR', // CYPRUS
+  'CZ': 'CZK', // CZECH REPUBLIC (THE)
+  'CI': 'XOF', // CÔTE D'IVOIRE
+  'DK': 'DKK', // DENMARK
+  'DJ': 'DJF', // DJIBOUTI
+  'DM': 'XCD', // DOMINICA
+  'DO': 'DOP', // DOMINICAN REPUBLIC (THE)
+  'EC': 'USD', // ECUADOR
+  'EG': 'EGP', // EGYPT
+  'SV': 'USD', // EL SALVADOR
+  'GQ': 'XAF', // EQUATORIAL GUINEA
+  'ER': 'ERN', // ERITREA
+  'EE': 'EUR', // ESTONIA
+  'ET': 'ETB', // ETHIOPIA
+  // 'EU': 'EUR', // EUROPEAN UNION
+  'FK': 'FKP', // FALKLAND ISLANDS (THE) [MALVINAS]
+  'FO': 'DKK', // FAROE ISLANDS (THE)
+  'FJ': 'FJD', // FIJI
+  'FI': 'EUR', // FINLAND
+  'FR': 'EUR', // FRANCE
+  'GF': 'EUR', // FRENCH GUIANA
+  'PF': 'XPF', // FRENCH POLYNESIA
+  // 'TF': '', // FRENCH SOUTHERN TERRITORIES (THE)
+  'GA': 'XAF', // GABON
+  'GM': 'GMD', // GAMBIA (THE)
+  'GE': 'GEL', // GEORGIA
+  'DE': 'EUR', // GERMANY
+  'GH': 'GHS', // GHANA
+  'GI': 'GIP', // GIBRALTAR
+  'GR': 'EUR', // GREECE
+  // 'GL': '', // GREENLAND
+  'GD': 'XCD', // GRENADA
+  'GP': 'EUR', // GUADELOUPE
+  'GU': 'USD', // GUAM
+  'GT': 'GTQ', // GUATEMALA
+  'GG': 'GBP', // GUERNSEY
+  'GN': 'GNF', // GUINEA
+  'GW': 'XOF', // GUINEA-BISSAU
+  'GY': 'GYD', // GUYANA
+  'HT': 'HTG', // HAITI
+  'HN': 'HNL', // HONDURAS
+  'HK': 'HKD', // HONG KONG
+  'HU': 'HUF', // HUNGARY
+  'IS': 'ISK', // ICELAND
+  'IN': 'INR', // INDIA
+  'ID': 'IDR', // INDONESIA
+  // 'XDR': '', // INTERNATIONAL MONETARY FUND (IMF) - SDR (Special Drawing Right)
+  'IR': 'IRR', // IRAN (ISLAMIC REPUBLIC OF)
+  'IQ': 'IQD', // IRAQ
+  'IE': 'EUR', // IRELAND
+  // 'IM': '', // ISLE OF MAN
+  'IL': 'ILS', // ISRAEL
+  'IT': 'EUR', // ITALY
+  'JM': 'JMD', // JAMAICA
+  'JP': 'JPY', // JAPAN
+  // 'JE': '', // JERSEY
+  'JO': 'JOD', // JORDAN
+  'KZ': 'KZT', // KAZAKHSTAN
+  'KE': 'KES', // KENYA
+  'KI': 'AUD', // KIRIBATI
+  'KP': 'KPW', // KOREA (THE DEMOCRATIC PEOPLE’S REPUBLIC OF)
+  'KR': 'KRW', // KOREA (THE REPUBLIC OF)
+  'KW': 'KWD', // KUWAIT
+  'KG': 'KGS', // KYRGYZSTAN
+  'LA': 'LAK', // LAO PEOPLE’S DEMOCRATIC REPUBLIC (THE)
+  'LV': 'EUR', // LATVIA
+  'LB': 'LBP', // LEBANON
+  'LS': 'LSL', // LESOTHO
+  // 'LS': 'ZAR', // LESOTHO (Rand)
+  'LR': 'LRD', // LIBERIA
+  'LY': 'LYD', // LIBYA
+  'LI': 'CHF', // LIECHTENSTEIN
+  'LT': 'EUR', // LITHUANIA
+  'LU': 'EUR', // LUXEMBOURG
+  'MO': 'MOP', // MACAO
+  'MG': 'MGA', // MADAGASCAR
+  'MW': 'MWK', // MALAWI
+  'MY': 'MYR', // MALAYSIA
+  'MV': 'MVR', // MALDIVES
+  'ML': 'XOF', // MALI
+  'MT': 'EUR', // MALTA
+  // 'MH': '', // MARSHALL ISLANDS (THE)
+  'MQ': 'EUR', // MARTINIQUE
+  'MR': 'MRU', // MAURITANIA
+  'MU': 'MUR', // MAURITIUS
+  // 'YT': '', // MAYOTTE
+  // 'XUA': '', // MEMBER COUNTRIES OF THE AFRICAN DEVELOPMENT BANK GROUP - ADB Unit of Account
+  'MX': 'MXN', // MEXICO
+  'MXV': 'MXV', // MEXICO - Mexican Unidad de Inversion (UDI)
+  // 'FM': '', // MICRONESIA (FEDERATED STATES OF)
+  'MD': 'MDL', // MOLDOVA (THE REPUBLIC OF)
+  // 'MC': '', // MONACO
+  'MN': 'MNT', // MONGOLIA
+  // 'ME': '', // MONTENEGRO
+  'MS': 'XCD', // MONTSERRAT
+  'MA': 'MAD', // MOROCCO
+  'MZ': 'MZN', // MOZAMBIQUE
+  'MM': 'MMK', // MYANMAR
+  'NA': 'NAD', // NAMIBIA
+  // 'NA': 'ZAR', // NAMIBIA (Rand)
+  'NR': 'AUD', // NAURU
+  'NP': 'NPR', // NEPAL
+  'NL': 'EUR', // NETHERLANDS (THE)
+  'NC': 'XPF', // NEW CALEDONIA
+  'NZ': 'NZD', // NEW ZEALAND
+  'NI': 'NIO', // NICARAGUA
+  'NE': 'XOF', // NIGER (THE)
+  'NG': 'NGN', // NIGERIA
+  'NU': 'NZD', // NIUE
+  // 'NF': '', // NORFOLK ISLAND
+  // 'MP': '', // NORTHERN MARIANA ISLANDS (THE)
+  'NO': 'NOK', // NORWAY
+  'OM': 'OMR', // OMAN
+  'PK': 'PKR', // PAKISTAN
+  // 'PW': '', // PALAU
+  // 'PS': '', // PALESTINE, STATE OF
+  'PA': 'PAB', // PANAMA
+  // 'PA': 'USD', // PANAMA
+  'PG': 'PGK', // PAPUA NEW GUINEA
+  'PY': 'PYG', // PARAGUAY
+  'PE': 'PEN', // PERU
+  'PH': 'PHP', // PHILIPPINES (THE)
+  'PN': 'NZD', // PITCAIRN
+  'PL': 'PLN', // POLAND
+  'PT': 'EUR', // PORTUGAL
+  'PR': 'USD', // PUERTO RICO
+  'QA': 'QAR', // QATAR
+  'MK': 'MKD', // REPUBLIC OF NORTH MACEDONIA
+  'RO': 'RON', // ROMANIA
+  'RU': 'RUB', // RUSSIAN FEDERATION (THE)
+  'RW': 'RWF', // RWANDA
+  'RE': 'EUR', // RÉUNION
+  'BL': 'EUR', // SAINT BARTHÉLEMY
+  'SH': 'SHP', // SAINT HELENA, ASCENSION AND TRISTAN DA CUNHA
+  'KN': 'XCD', // SAINT KITTS AND NEVIS
+  'LC': 'XCD', // SAINT LUCIA
+  'MF': 'EUR', // SAINT MARTIN (FRENCH PART)
+  'PM': 'EUR', // SAINT PIERRE AND MIQUELON
+  'VC': 'XCD', // SAINT VINCENT AND THE GRENADINES
+  'WS': 'WST', // SAMOA
+  'SM': 'EUR', // SAN MARINO
+  'ST': 'STN', // SAO TOME AND PRINCIPE
+  'SA': 'SAR', // SAUDI ARABIA
+  'SN': 'XOF', // SENEGAL
+  'RS': 'RSD', // SERBIA
+  'SC': 'SCR', // SEYCHELLES
+  'SL': 'SLL', // SIERRA LEONE
+  'SG': 'SGD', // SINGAPORE
+  'SX': 'ANG', // SINT MAARTEN (DUTCH PART)
+  'SK': 'EUR', // SLOVAKIA
+  'SI': 'EUR', // SLOVENIA
+  'SB': 'SBD', // SOLOMON ISLANDS
+  'SO': 'SOS', // SOMALIA
+  'ZA': 'ZAR', // SOUTH AFRICA
+  // 'GS': '', // SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS
+  'SS': 'SSP', // SOUTH SUDAN
+  'ES': 'EUR', // SPAIN
+  'LK': 'LKR', // SRI LANKA
+  'PS': 'ILS', // STATE OF PALESTINE
+  'SD': 'SDG', // SUDAN (THE)
+  'SR': 'SRD', // SURINAME
+  'SJ': 'NOK', // SVALBARD AND JAN MAYEN
+  'SE': 'SEK', // SWEDEN
+  'CH': 'CHF', // SWITZERLAND
+  'SY': 'SYP', // SYRIAN ARAB REPUBLIC
+  'TW': 'TWD', // TAIWAN (PROVINCE OF CHINA)
+  'TJ': 'TJS', // TAJIKISTAN
+  'TZ': 'TZS', // TANZANIA, UNITED REPUBLIC OF
+  'TH': 'THB', // THAILAND
+  'TL': 'USD', // TIMOR-LESTE
+  'TG': 'XOF', // TOGO
+  'TK': 'NZD', // TOKELAU
+  'TO': 'TOP', // TONGA
+  'TT': 'TTD', // TRINIDAD AND TOBAGO
+  'TN': 'TND', // TUNISIA
+  'TR': 'TRY', // TURKEY
+  'TM': 'TMT', // TURKMENISTAN
+  'TC': 'USD', // TURKS AND CAICOS ISLANDS (THE)
+  'TV': 'AUD', // TUVALU
+  'UG': 'UGX', // UGANDA
+  'UA': 'UAH', // UKRAINE
+  'AE': 'AED', // UNITED ARAB EMIRATES (THE)
+  'GB': 'GBP', // UNITED KINGDOM OF GREAT BRITAIN AND NORTHERN IRELAND (THE)
+  'UM': 'USD', // UNITED STATES MINOR OUTLYING ISLANDS (THE)
+  'US': 'USD', // UNITED STATES OF AMERICA (THE)
+  'UY': 'UYU', // URUGUAY
+  'UZ': 'UZS', // UZBEKISTAN
+  'VU': 'VUV', // VANUATU
+  'VE': 'VES', // VENEZUELA (BOLIVARIAN REPUBLIC OF)
+  'VN': 'VND', // VIET NAM
+  'VG': 'USD', // VIRGIN ISLANDS (BRITISH)
+  'VI': 'USD', // VIRGIN ISLANDS (U.S.)
+  'WF': 'XPF', // WALLIS AND FUTUNA
+  'EH': 'MAD', // WESTERN SAHARA
+  'YE': 'YER', // YEMEN
+  'ZM': 'ZMW', // ZAMBIA
+  'ZW': 'ZWL', // ZIMBABWE
+};
+
