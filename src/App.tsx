8882cd5d5fc8b8061845f2305159900794254ae5
nidnogg <henriquevt98@gmail.com>
2024-02-14 01:23:23 -0300

    Some mods on frontend

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p className="subtitle">
+          Compare currencies based on values you find expensive or cheap.
         </p>
+        <p>Start by selecting your home country</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
