79b6c3b47779487ab6ffdb6d00f04e1ddfb2881c
nidnogg <henriquevt98@gmail.com>
2024-03-05 20:08:19 -0300

    Added new comboboxes

--- src/App.tsx
+++ src/App.tsx
@@ -228,7 +228,7 @@ function App() {
     <>
       <div ref={parent}>
         <Header />
-        <p className="subtitle">
+        <p className="mt-5 subtitle">
           {getSubtitleText()}
         </p>
         {selectedCountry && (
@@ -265,8 +265,8 @@ function App() {
             }
 
             <br />
-            {/* Maybe componentify this too? */}
-
+            {/* TO-DO remove this */}
+            {/* 
             <select
               value={""}
               onChange={(e) => {
@@ -281,31 +281,16 @@ function App() {
               ))}
             </select>
 
-
-          </div>
-
-        )}
-        {!selectedCountry && (
-          <div>
-            <p className="mt-5">Start by selecting your <b>home country</b></p>
-            {/* 
-            <select
-              value={selectedCountry}
-              onChange={(e) => setSelectedCountry(e.target.value)}
-            >
-              <option value="">Select Country</option>
-              {Object.entries(countryNamesCountryCodes).map(([countryAndCurrencyName, countryCode]) => (
-                <option key={countryAndCurrencyName} value={countryCode}>
-                  {countryAndCurrencyName}
-                </option>
-              ))}
-            </select>
             */}
-            <Combobox value={selectedCountry} onChange={(value) => setSelectedCountry(value)}>
+            {/* Add Another Country Combobox - TO-DO move this into component */}
+            <Combobox value={""} onChange={(value) => {
+              setSelectedCountriesToCompare([...selectedCountriesToCompare, value as string]);
+            }}>
               <div className="relative mt-1">
-                <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                <div className="relative w-full cursor-default overflow-hidden rounded-lg text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
                   <Combobox.Input
                     className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 focus:ring-0"
+                    placeholder="Add another Country to Compare"
                     onChange={(event) => setQuery(event.target.value)}
                   />
                   <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
@@ -322,7 +307,7 @@ function App() {
                   leaveTo="opacity-0"
                   afterLeave={() => setQuery('')}
                 >
-                  <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                  <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-gray-900 text-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
                     {filteredCountries.length === 0 && query !== '' ? (
                       <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                         Nothing found.
@@ -332,7 +317,86 @@ function App() {
                         <Combobox.Option
                           key={country.id}
                           className={({ active }) =>
-                            `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-gray-700 text-white' : 'text-white-900'}`
+                          }
+                          value={country.id}
+                        >
+                          {({ selected, active }) => (
+                            <>
+                              <span
+                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}
+                              >
+                                {country.name}
+                              </span>
+                              {selected ? (
+                                <span
+                                  className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'}`}
+                                >
+                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Combobox.Option>
+                      ))
+                    )}
+                  </Combobox.Options>
+                </Transition>
+              </div>
+            </Combobox>
+          </div>
+
+        )}
+        {!selectedCountry && (
+          <div>
+            <p className="mt-5">Start by selecting your <b>home country</b></p>
+            {/* TO-DO remove this */}
+            {/* 
+            <select
+              value={selectedCountry}
+              onChange={(e) => setSelectedCountry(e.target.value)}
+            >
+              <option value="">Select Country</option>
+              {Object.entries(countryNamesCountryCodes).map(([countryAndCurrencyName, countryCode]) => (
+                <option key={countryAndCurrencyName} value={countryCode}>
+                  {countryAndCurrencyName}
+                </option>
+              ))}
+            </select>
+            */}
+            <Combobox value={selectedCountry} onChange={(value) => setSelectedCountry(value)}>
+              <div className="relative mt-1">
+                <div className="relative w-full cursor-default overflow-hidden rounded-lgtext-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                  <Combobox.Input
+                    className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 focus:ring-0"
+                    placeholder="Here be nations"
+                    onChange={(event) => setQuery(event.target.value)}
+                  />
+                  <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </Combobox.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                  afterLeave={() => setQuery('')}
+                >
+                  <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-gray-900 text-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                    {filteredCountries.length === 0 && query !== '' ? (
+                      <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                        Nothing found.
+                      </div>
+                    ) : (
+                      filteredCountries.map((country) => (
+                        <Combobox.Option
+                          key={country.id}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-gray-700 text-white' : 'text-white-900'
                             }`
                           }
                           value={country.id}
